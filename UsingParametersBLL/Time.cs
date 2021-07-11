a5ddbb9298ee19cc9dd806d4bd94afac862ce995
Ferdinand Teguefouet <teguefouet1@gmail.com>
2021-07-11 02:43:29 +0200

    Ajoutez des fichiers projet.

---
+++ UsingParametersBLL/Time.cs
@@ -0,0 +1,103 @@
+﻿
+namespace UsingParametersBLL
+{
+    public class Time
+    {
+        #region Constants, fields and properties
+        private const int m_COEFFMINUTES = 60;
+        private static int m_TotalMinutes;
+        private int m_Hour;
+        private int Hour
+        {
+            get { return m_Hour; }
+            set { m_Hour = value; }
+        }
+
+        private int m_Minute;
+        private int Minute
+        {
+            get { return m_Minute; }
+            set { m_Minute = value; }
+        } 
+        #endregion
+
+        #region Constructors
+        public Time(int PIntTime)
+        {
+            m_TotalMinutes = PIntTime;
+        }
+        public Time(int pHour, int pMinute)
+        {
+            m_Hour = pHour;
+            m_Minute = pMinute;
+        } 
+        #endregion
+
+        #region Operators overloading
+        public static Time operator +(Time pTime1, Time pTime2)
+        {
+            m_TotalMinutes = (pTime1.Hour + pTime2.Hour) * m_COEFFMINUTES + pTime1.Minute + pTime2.Minute;
+            return CalculateHourAndMinute();
+        }
+
+        public static Time operator -(Time pTime1, Time pTime2)
+        {
+            m_TotalMinutes = pTime1.Hour * m_COEFFMINUTES + pTime1.Minute - (pTime2.Hour * m_COEFFMINUTES + pTime2.Minute);
+            return CalculateHourAndMinute();
+        }
+
+        public static Time operator *(Time pTime3, int pCoeff)
+        {
+            m_TotalMinutes = (pTime3.Hour * m_COEFFMINUTES + pTime3.Minute) * pCoeff;
+            return CalculateHourAndMinute();
+        }
+
+        public static Time operator /(Time pTime3, int pCoeff)
+        {
+            m_TotalMinutes = (pTime3.Hour * m_COEFFMINUTES + pTime3.Minute) / pCoeff;
+            return CalculateHourAndMinute();
+        } 
+        #endregion
+
+        #region Overloading conversion operators
+
+        public static explicit operator Time(int PIntToTime) => new Time(PIntToTime / m_COEFFMINUTES, PIntToTime % m_COEFFMINUTES);
+
+        public static explicit operator int(Time pTime1) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute;
+
+        public static explicit operator float(Time pTime1) => (float)(pTime1.Hour * m_COEFFMINUTES + pTime1.Minute) / m_COEFFMINUTES;
+
+        public static explicit operator Time(float pTimeFloat) => new Time((int)(pTimeFloat * m_COEFFMINUTES) / m_COEFFMINUTES,
+            (int)(pTimeFloat * m_COEFFMINUTES) % m_COEFFMINUTES);
+        #endregion
+
+        #region Overloading relational operators
+        public static bool operator <(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute < pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+        public static bool operator >(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute > pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+        public static bool operator <=(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute <= pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+        public static bool operator >=(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute >= pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+
+        public static bool operator ==(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute == pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+        public static bool operator !=(Time pTime1, Time pTime2) => pTime1.Hour * m_COEFFMINUTES + pTime1.Minute != pTime2.Hour * m_COEFFMINUTES + pTime2.Minute;
+        #endregion
+
+        #region Methods
+        private static Time CalculateHourAndMinute() => new Time(m_TotalMinutes / m_COEFFMINUTES, m_TotalMinutes % m_COEFFMINUTES);
+
+        public override string ToString() => $"{Hour:##00}:{Minute:00} min";
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public override bool Equals(object PTime)
+        {
+            var myTime = (Time)PTime;
+            if (myTime.Hour == this.Hour && myTime.Minute == this.Minute)
+                return true;
+            else
+                return false;
+        }
+        #endregion
+    }
+}
---
+++ UsingParameters_New/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UsingParametersBLL;
+
+namespace UsingParameters_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1, time2;
+            time1 = new Time(2, 30);
+            time2 = new Time(3, 15);
+            int TimeInt = 120;
+            int coefficient = 2;
+            float timeFloat= 12.5215354f;
+
+            Console.WriteLine("++++++++++++++++ Operators overloading +++++++++++++++++++++");
+
+            Console.WriteLine($"{time1} + {time2} = {time1 + time2}");
+            Console.WriteLine($"{time1} - {time2} = {time1 - time2}");
+            Console.WriteLine($"{time1} * {coefficient} = {time1 * coefficient}");
+            Console.WriteLine($"{time2} / {coefficient} = {time2 / coefficient}");
+
+            Console.WriteLine("++++++++++++++++ Overloading conversion operators ++++++++++");
+
+            Console.WriteLine($"Int ({TimeInt}) en Time --> {(Time)TimeInt}");
+            Console.WriteLine($"Time ({time1}) en Int --> {(int)time1}");
+            Console.WriteLine($"Time ({time1}) en float --> {(float)time1}");
+            Console.WriteLine($"Float ({timeFloat}) en Time --> {(Time)timeFloat}");
+
+            Console.WriteLine("++++++++++++++++ Overloading relational operators ++++++++++");
+
+            Console.WriteLine($"Comparaison: {time1} < {time2} --> {time1 < time2}");
+            Console.WriteLine($"Comparaison: {time1} > {time2} --> {time1 > time2}");
+            Console.WriteLine($"Comparaison: {time1} <= {time2} --> {time1 <= time2}");
+            Console.WriteLine($"Comparaison: {time1} >= {time2} --> {time1 >= time2}");
+            Console.WriteLine($"Comparaison: {time1} == {time2} --> {time1 == time2}");
+            Console.WriteLine($"Comparaison: {time1} != {time2} --> {time1 != time2}");
+            //Console.WriteLine(tMinute.TotalMinutes);
+            //Console.WriteLine(time1.TotalMinutes);
+            //Console.WriteLine(time2.TotalMinutes);
+        }
+    }
+}
